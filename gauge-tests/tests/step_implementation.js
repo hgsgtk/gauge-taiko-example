a4bd9402676e1021d544fa2ecd8b355d6b790dda
hgsgtk <hgsgtk@gmail.com>
2021-03-14 23:03:05 +0900

    feat: プロポーザル一覧を見る

--- gauge-tests/tests/step_implementation.js
+++ gauge-tests/tests/step_implementation.js
@@ -52,6 +52,15 @@ step("View <type> tasks", async function (type) {
     await click(link(type));
 });
 
+step("Step を実行する", async function () {
+    await press('Step');
+});
+
+step("パラメータ <param> を渡して、Step を実行する", async function (param) {
+    await write(paramm, into(textBox("Write param here")));
+    await press('Step');
+});
+
 step("Complete tasks <table>", async function (table) {
     for (var row of table.rows) {
         await click(checkBox(toLeftOf(row.cells[0])));
@@ -90,22 +99,58 @@ step("Must have <table>", async function (table) {
 });
 
 step("<arg0> 件プロポーザルを出したスピーカーは、自身が提出したプロポーザルの一覧で <arg1> 件のプロポーザルを確認できる", async function(arg0, arg1) {
+    assert.ok(true)
 });
 
 step("プロポーザルの一覧にて次の情報を表示される <arg0>", async function(arg0) {
+    assert.ok(true)
 });
 
 step("トーク時間に応じて収録開始・収録終了が表示される <arg0>", async function(arg0) {
+    assert.ok(true)
 });
 
 step("レコーディング予約中のスピーカーは、レコーディング予約をキャンセルすることが出来る", async function() {
+    assert.ok(true)
 });
 
 step("カンファレンス運営者は、採択済みのプロポーザルに対して、管理画面からメールを送信出来る", async function() {
+    assert.ok(true)
 });
 
 step("採択されたスピーカーはメールを受け取り、採択可否の返答画面へアクセスできる", async function() {
+    assert.ok(true)
 });
 
 step("プロポーザル提出者へ採択承諾のリマインドを送る <arg0>", async function(arg0) {
+    // PHPUnitでのテストがチェックインされるまで
+    throw 'Untested Scenario by PHPUnit';
+    // チェックインされたら
+    assert.ok(true, '当該UnitTestへのGitHub URL');
+});
+
+step("プロポーザル一覧画面を見る", async function() {
+    // プロポーザル一覧画面へ遷移する
+    await goto('https://fortee.jp/phperkaigi-2021/speaker/proposal/list')
+});
+
+step("タイトル <talkTitle> のプロポーザル詳細へ移動する", async function(talkTitle) {
+	// 該当トークタイトルのプロポーザルのリンクをクリック
+    await click(/* 指定条件 */)
+});
+
+step("<recordingTime> にレコーディング予約する", async function(recordingTime) {
+	throw 'Unimplemented Step';
+});
+
+step("プロポーザル詳細画面でレコーディング時間に応じて収録開始・収録終了が表示される <arg0>", async function(arg0) {
+	throw 'Unimplemented Step';
+});
+
+step("プロポーザル詳細画面からレコーディング予約をキャンセルする", async function() {
+	throw 'Unimplemented Step';
+});
+
+step("手動テスト", async function() {
+	assert.ok(true)
 });
